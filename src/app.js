c24cb27a9e3d6177cc041942f911c5efb08eeb26
Emanuel Silva <emanuel.silva2407@gmail.com>
2025-02-14 15:46:28 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
 import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateExcuse() {
+  let who = ["Mi perro", "Mi mamá", "Un extraterrestre", "Mi vecino", "Un fantasma", "Mi profesor"];
+  let action = ["se comió", "destruyó", "robó", "perdió", "quemó"];
+  let what = ["mi tarea", "mi proyecto", "mi teléfono", "mis llaves", "mi ordenador"];
+  let when = ["ayer", "esta mañana", "hace un rato", "el otro día", "mientras dormía"];
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("excuse").innerHTML = generateExcuse();
+});
